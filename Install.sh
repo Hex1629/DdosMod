ca89544933a9b820b933317201182b66f30d4bf1
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2021-12-01 07:14:36 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,7 +3,10 @@ apt upgrade
 apt install python
 apt install git
 apt install dnsutils
-pkh install tree
+pkg install tree
+pkg install python3
+pkg install python2
+pkg install git
 pkg install ruby
 gem install lolcat
 pkg install wget -y
